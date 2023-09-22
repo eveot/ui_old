67ac7c8c57ceea41d9a1a3b4e42e3665e75474ef
IntarialN <support@intarial.com>
2023-09-22 12:18:58 +0300

    [DEV-4] Настройка rollup под использование библиотеки

--- dist/components/Checkbox/Checkbox.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Checkbox.less';
-export interface ButtonProps {
-    onClick: () => void;
-    label: string;
-    isActive: boolean;
-}
-declare const Checkbox: ({ isActive, onClick, label, ...props }: ButtonProps) => React.JSX.Element;
-export default Checkbox;
--- dist/components/Input/Input.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import './Input.less';
-export interface ButtonProps {
-    onChange: (value: string) => void;
-    placeholder?: string;
-    size?: 'sm' | 'md';
-}
-declare const Input: ({ size, placeholder, onChange, ...props }: ButtonProps) => React.JSX.Element;
-export default Input;
