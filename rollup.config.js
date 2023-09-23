0b01a780feda375fa256f9128ca7b5f17a4bd69f
IntarialN <support@intarial.com>
2023-09-23 11:46:42 +0300

    [DEV-4] Обновление версии через actions

--- rollup.config.js
+++ rollup.config.js
@@ -4,24 +4,27 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import serve from 'rollup-plugin-serve';
 import less from 'rollup-plugin-less';
-import css from 'rollup-plugin-css-only';
+const pkg = require("./package.json");
 
 export default {
     input: 'src/index.ts',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'iife',
-    },
+    output: [
+        { file: pkg.main, format: "cjs" },
+        { file: pkg.module, format: "esm" }
+    ],
     plugins: [
         resolve(),
         commonjs(),
         typescript({
-            tsconfig: './tsconfig.json',
+            tsconfig: "./tsconfig.json",
+            outDir: ".",
+            include: ["src/**/*.tsx", "src/**/*.ts"],
+            exclude: ["src/**/*.stories.tsx"]
         }),
         less({
             insert: true,
+            output: "dist/styles/eveot.min.css",
         }),
-        css({ output: 'bundle.css' }),
         babel({
             extensions: ['.tsx', '.less'],
             exclude: 'node_modules/**',
